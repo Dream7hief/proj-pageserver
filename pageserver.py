d1fc3d14d44e935c931d91d0822152bdd029e411
Dream7hief <logan.poole1123@gmail.com>
2016-09-30 19:42:42 -0700

    v2

--- pageserver.py
+++ pageserver.py
@@ -84,12 +84,12 @@ def respond(sock):
     if len(parts) > 1 and parts[0] == "GET":
         
         try:
-            with open('./pages/{}'.format(PagePath)) as f:
+            with open('./pages'.format(PagePath)) as f:
                 html = f.read()
                 transmit(STATUS_OK, sock)
                 transmit(html, sock)
         except Exception:
-            transmit(STATUS_NOT_FOUND, sock)
+            transmit("503 Server Error", sock)
         
     else:
         transmit(STATUS_NOT_IMPLEMENTED, sock)        
